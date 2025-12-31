efcaea54c1263c5a0a91778841be0cb08bd11a04
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-12-31 15:29:50 +0100

    updated linux links

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports.config = {
 module.exports = function(eleventyConfig) {
 	// Order matters, put this at the top of your configuration file.
     eleventyConfig.addPassthroughCopy('src/style.css');
-    eleventyConfig.addPassthroughCopy('js/dropdown.js');
+    eleventyConfig.addPassthroughCopy('src/js');
     eleventyConfig.addPassthroughCopy('src/assets/')
   
     return {
